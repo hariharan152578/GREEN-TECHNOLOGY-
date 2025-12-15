6eba1595ae795c6029c2a282fc99163982a7e8d5
hariharan152578 <hariharan152578@gmail.com>
2025-12-15 09:23:13 +0530

    create Home layout with hero and about section

--- client/src/Routes/Approutes.tsx
+++ client/src/Routes/Approutes.tsx
@@ -1,10 +1,15 @@
 import { Routes , Route} from "react-router-dom"
 import Home from "../pages/home/layout/Home"
+import Course from "../pages/course/layout/Course"
+import { MainLayout } from "../layout/MainLayout"
 
 const Approutes = () => {
   return (
     <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<MainLayout/>}>
+            <Route index element={<Home/>}/>
+            <Route path="/course" element={<Course/>}/>
+        </Route>
     </Routes>
   )
 }
--- client/src/pages/home/layout/Home.tsx
+++ client/src/pages/home/layout/Home.tsx
@@ -1,8 +1,16 @@
+import React from 'react';
+import HeroSection from '../section/HeroSection'; 
+import AboutSection from '../section/AboutSection';
 
-const Home = () => {
+const Home: React.FC = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="min-h-screen bg-white">
+      <main>
+        <HeroSection/>
+        <AboutSection/>
+      </main>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
